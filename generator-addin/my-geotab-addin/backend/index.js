f590ff5066e6c2eceea1565bddf77541f18b76b1
Dhruv popat <dhruvpopat268@gmail.com>
2025-05-01 18:02:18 +0530

    backend

--- generator-addin/my-geotab-addin/backend/index.js
+++ generator-addin/my-geotab-addin/backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 7000
+const port = process.env.PORT || 7000
 const connectToDb = require('./database/db')
 const driverRoutes = require('./Routes/driverRoute')
 
